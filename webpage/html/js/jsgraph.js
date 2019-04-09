854a01fb12ca9ee6c8a7f0387ac60421abc362e6
yching1 <yching@life.maryville.edu>
2019-04-09 16:04:29 -0400

    Changed from CanvasJS to ChartJS (free). Added colorblind palette. Graphics not tested yet.

--- webpage/html/js/jsgraph.js
+++ webpage/html/js/jsgraph.js
@@ -19,41 +19,104 @@ function createGraphFromJSON(queryresult,container,varx,agraphtype) {
 
     var graphtype = "";
     var data = [];
-    var adatum = {};
-    var datapoints = [];
-    var adatapoint = {};
-
+    var adatum = [];
+    var alldatasets = [];
+    var adataset = {};
     graphtype = agraphtype;
+    var xlabels = [];
+
+    var chartColors15 = [
+        'rgba(0, 0, 0, 1)', 
+        'rgba(0, 73, 73, 1)',
+        'rgba(0, 146, 146, 1)',
+        'rgba(255, 109, 182, 1)',
+        'rgba(73, 0, 146, 1)',
+        'rgba(0, 109, 219, 1)',
+        'rgba(182, 109, 255, 1)',
+        'rgba(109, 182, 255, 1)',
+        'rgba(255, 182, 119, 1)',
+        'rgba(182, 219, 255, 1)',
+        'rgba(146, 0, 0, 1)',
+        'rgba(146, 73, 0, 1)',
+        'rgba(219, 209, 0, 1)',
+        'rgba(36, 255, 36, 1)',
+        'rgba(255, 255, 109, 1)'
+    ];
+    var chartColors6 = [
+        'rgba(255, 255, 255, 1)', //white
+        'rgba(0, 0, 0, 1)', //black
+        'rgba(255, 127, 0, 1)', //orange
+        'rgba(255, 255, 0, 1)', //yellow
+        'rgba(0, 0, 255, 1)', //blue
+        'rgba(127, 127, 127, 1)' //grey
+    ];
+
+    coloridx = -1;
+
+    // Populate dataset for the graphs
     for (var i = 0; i < col.length; i++) {
-        if (col[i] === varx) {
-            //do nothing;
-        } else {
-            adatapoint.length = 0;
-            adatum.length = 0;
-            for (var j = 0; j < JSONqueryresult.length; j++) {
-                console.log(JSONqueryresult[j][col[i]]);
-                adatapoint = {
-                    x: JSONqueryresult[j][col[idxvarx]],
-                    y: JSONqueryresult[j][col[i]]
-                };
-                datapoints.push(adatapoint);
-//                console.log("datapoints: %0", datapoints);
-                console.log(JSON.stringify(datapoints));
+        adatapoint.length = 0;
+        adatum.length = 0;
+        for (var j = 0; j < JSONqueryresult.length; j++) {
+            if (col[i] === varx) {
+                // The X values are stored as xlabels
+                xlabels.push(JSONqueryresult[j][col[i]])
+            } else { 
+                // data: [y0,y1,...];
+                console.log("adatum: %0: ", JSONqueryresult[j][col[i]]);
+                adatum.push(JSONqueryresult[j][col[i]]);
             }
-            datum = {
-                name: col[i],
-		type: graphtype,
-//		yValueFormatString: "#0.## °C",
-		showInLegend: true,
-		dataPoints: datapoints
+        }  
+        if (col[i] === varx) {
+        } else {
+            coloridx += 1;
+            if (coloridx > 14) {
+                coloridx = 0;
+            }
+            // data structure for each Y-variable
+            adataset = {
+                label: col[i],
+		backgroundColor: chartColors15[coloridx],
+                borderColor: 'rgba(255,255,255,1)',
+                borderWidth: 1,
+		data: adatum
             };
-            console.log(datum);
-            data.push(datum);
-//            console.log("data: %0", data);
-            console.log(JSON.stringify(data));
+            console.log(adataset);
+            alldatasets.push(adataset);
+            console.log(JSON.stringify(alldatasets));
         }
     }
+    var chartData = {
+        labels: xlabels,
+        datasets: alldatasets        
+    };
 
+    var myChart = {
+        type: graphtype.
+        data: chartData,
+        options: {
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart - Stacked'
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    stacked: true,
+                }],
+                yAxes: [{
+                    stacked: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    };
 
     // CREATE DYNAMIC GRAPH.
     var table = document.createElement("table");
@@ -81,4 +144,7 @@ function createGraphFromJSON(queryresult,container,varx,agraphtype) {
     var divContainer = document.getElementById(container);
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
+
+    var graphic = document.createElement("agraph");
+
 }
--- webpage/html/js/jsgraphcanvas.js
+++ webpage/html/js/jsgraphcanvas.js
@@ -1,17 +1,63 @@
-function createGraphFromJSON(queryresult,container,varx) {
+function createGraphFromJSON(queryresult,container,varx,agraphtype) {
     console.log("Creating graph...");
     var JSONqueryresult = JSON.parse(queryresult);
     console.log(JSONqueryresult.length);
     var count = Object.keys(JSONqueryresult).length;
     console.log(count);
     var col = [];
+    var idxvarx;
     for (var i = 0; i < JSONqueryresult.length; i++) {
         for (var key in JSONqueryresult[i]) {
             if (col.indexOf(key) === -1) {
                 col.push(key);
+                if (key === varx) {
+                    idxvarx = i;
+                }
             }
         }
     }
+
+    var graphtype = "";
+    var data = [];
+    var adatum = {};
+    var datapoints = [];
+    var adatapoint = {};
+
+    graphtype = agraphtype;
+
+    // Populate datapoints for the graphs
+    for (var i = 0; i < col.length; i++) {
+        if (col[i] === varx) {
+            // do nothing;
+            // The X variable cannot be Y variable
+        } else { 
+            // datapoint: [{x:,y:}];
+            adatapoint.length = 0;
+            adatum.length = 0;
+            for (var j = 0; j < JSONqueryresult.length; j++) {
+                console.log(JSONqueryresult[j][col[i]]);
+                adatapoint = {
+                    x: JSONqueryresult[j][col[idxvarx]],
+                    y: JSONqueryresult[j][col[i]]
+                };
+                datapoints.push(adatapoint);
+                console.log(JSON.stringify(datapoints));
+            }
+            // data structure for each Y-variable
+            datum = {
+                name: col[i],
+		type: graphtype,
+//		yValueFormatString: "#0.## °C",
+		showInLegend: true,
+		dataPoints: datapoints
+            };
+            console.log(datum);
+            data.push(datum);
+            console.log(JSON.stringify(data));
+        }
+    }
+
+
     // CREATE DYNAMIC GRAPH.
     var table = document.createElement("table");
     // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
@@ -38,4 +84,7 @@ function createGraphFromJSON(queryresult,container,varx) {
     var divContainer = document.getElementById(container);
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
+
+    var graphic = document.createElement("agraph");
+
 }
